36a172692fea5a232eb9d76bf6ea54fc49fc25b9
Bintang Triadmaja <95063290+AmdaaRaijen@users.noreply.github.com>
2026-06-01 04:54:35 +0000

    docs: add comment to explain the I/O setup

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const readline = require("readline");
 
+/** 
+ * NOTE
+ * Program ini dibuat dengan javascript yang dijalankan oleh NodeJS
+ * 
+ * NodeJS sendiri berjalan diatas event loop yaitu sati trheat yang tidak bisa mengunggu.
+ * Jika program ini dipaksaa untuk menunggu input, event loop akan mati dan program tidak bisa lanjut
+ * 
+ * Oleh karena itu untuk membaca input dan memberikan output (I/O) pada NodeJS ini dibuat async
+*/
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
